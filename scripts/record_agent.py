9f8943e8ee0f02c60f056ef7d8a9ddb8ff7e8835
Weronika Jamróz <weronika.jamz@gmail.com>
2026-01-27 10:39:48 +0100

    make additional recordings

--- scripts/record_agent.py
+++ scripts/record_agent.py
@@ -35,10 +35,31 @@ def record_episodes(agent: PPOAgent, num_episodes=1, folder_path="recordings", n
 
 
 def record_random_behavior(num_episodes=1, folder_path="recordings", name_prefix="random-"):
-    pass
+    env = gym.make("CartPole-v1", render_mode="rgb_array")
+    env = RecordVideo(
+        env,
+        video_folder=folder_path,
+        name_prefix=name_prefix,
+        episode_trigger=lambda x: True
+    )
+
+    folder_path = Path(folder_path)
+    folder_path.mkdir(exist_ok=True, parents=True)
+
+    for i in range(num_episodes):
+        state, info = env.reset()
+        episode_over = False
+
+        while not episode_over:
+            action = env.action_space.sample()
+            state, reward, terminated, truncated, info = env.step(action)
+            episode_over = terminated or truncated
+
+    env.close()
 
 
 if __name__ == '__main__':
-    Agent = PPOAgent(4, 2, device)
-    Agent.load_model_from_dict("../models/Agent_0.pth")
-    record_episodes(Agent, 3, "../recordings", name_prefix="Agent_0-")
+    # Agent = PPOAgent(4, 2, device)
+    # Agent.load_model_from_dict("../models/Agent_0.pth")
+    # record_episodes(Agent, 3, "../recordings", name_prefix="Agent_0-")
+    record_random_behavior(2, "../recordings")
